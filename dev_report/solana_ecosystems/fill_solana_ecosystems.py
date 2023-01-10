308e9114fb80f5c7ab52bcc34b177d5c82398baa
0xphoton <42502920+maxtalwar@users.noreply.github.com>
2023-01-09 21:14:56 -0800

    created solana ecosystems checked
    wrote code to verify solana ecosystems

--- dev_report/solana_ecosystems/fill_solana_ecosystems.py
+++ dev_report/solana_ecosystems/fill_solana_ecosystems.py
@@ -2,27 +2,38 @@ import openpyxl
 import requests
 
 # Open the Excel file
-wb = openpyxl.load_workbook('file.xlsx')
+wb = openpyxl.load_workbook('tasks_solana.xlsx')
 
 # Select the sheet with the data
-sheet = wb['Sheet1']
+sheet = wb['Task 5 - Solana']
 
-# Iterate through each row in the sheet
-for row in sheet.rows:
-    # Get the name of the project from column C
-    project_name = row[2].value
+def alter_rows():
+    # Iterate through each row in the sheet
+    for row in sheet[5:336]:
+        # Get the name of the project from column C
+        project_name = row[2].value
 
-    # Make a request to the website
-    url = "https://defillama.com/chain/Solana"
-    response = requests.get(url)
+        # Make a request to the website
+        url = "https://defillama.com/chain/Solana"
+        response = requests.get(url)
 
-    # Check if the project name is in the website's HTML
-    if project_name in response.text:
-        # If it is, write "yes" to column G
-        row[6].value = "yes"
-    else:
-        # If it is not, write "no" to column G
-        row[6].value = "no"
+        # Check if the project name is in the website's HTML
+        if project_name in response.text:
+            # If it is, write "yes" to column G
+            row[6].value = "yes"
+        else:
+            # If it is not, write "no" to column G
+            row[6].value = "no"
 
-# Save the changes to the Excel file
-wb.save('file.xlsx')
+    # Save the changes to the Excel file
+    wb.save('tasks_solana.xlsx')
+
+def display_results():
+    for row in sheet[5:336]:
+        status = row[6].value
+
+        if status == "yes":
+            print(row[6].row)
+
+if __name__ == "__main__":
+    display_results()
